0cbd5e2d969e76fdcc41988ef07496ee795c58e8
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-04-06 16:11:34 +0530

    added files

--- app.js
+++ app.js
@@ -123,3 +123,12 @@ playAgainButton.addEventListener('click', () => {
 
 // Init
 displayWord();
+$('.inner-switch').on('click', function () {
+  if ($('body').hasClass('dark')) {
+    $('body').removeClass('dark');
+    $('.inner-switch').text('OFF');
+  } else {
+    $('body').addClass('dark');
+    $('.inner-switch').text('ON');
+  }
+});
